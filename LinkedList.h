4acf614573b99f5cb85473afe9b5142665735e68
Thomas Kent <wthomaskent@gmail.com>
2023-06-17 21:41:54 -0400

    Semi-functional LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -32,7 +32,7 @@ void PrintForwardRecursive(const Node* node) const;
 void PrintReverseRecursive(const Node* node) const;
 
 // Accessors
-unsigned int NodeCount () const;
+unsigned int NodeCount() const;
 void FindAll(vector<Node*>& outData, const T& value) const;
 const Node* Find(const T& data) const;
 Node* Find(const T& data);
@@ -70,7 +70,7 @@ LinkedList<T>& operator=(const LinkedList<T>& rhs);             // Copy assignme
 template <typename T>
 struct LinkedList<T>::Node
 {
-    T* _data;                                                   // Data stored in the node
+    T _data;                                                   // Data stored in the node
     Node* _next;                                                // Pointer to next node in linked list
     Node* _previous;                                            // Pointer to previous node in linked list
 };
@@ -84,9 +84,44 @@ LinkedList<T>::LinkedList()
     }
 
 template <typename T>
+LinkedList<T>::~LinkedList()
+{
+    Node* current_node = _head;
+
+    while (current_node != nullptr)
+    {
+        Node* next = current_node->_next;
+        delete current_node;
+        current_node = next;
+    }
+}
+
+template <typename T>
+void LinkedList<T>::PrintForward() const
+{
+    Node* current_item = _head;
+    for (unsigned int item = 0; item < _size; item++)
+    {
+        cout << current_item->_data << endl;
+        current_item = current_item->_next;
+    }
+}
+
+template <typename T>
+void LinkedList<T>::PrintReverse() const
+{
+    Node* current_item = _tail;
+    for (unsigned int item = 0; item < _size; item++)
+    {
+        cout << current_item->_data << endl;
+        current_item = current_item->_previous;
+    }
+}
+
+template <typename T>
 void LinkedList<T>::AddHead(const T& data)
 {
-    Node* new_head = new Node;          // New node to be added to front of list
+    Node* new_head = new Node();          // New node to be added to front of list
     new_head->_data = data;
 
     // Check to see if list is empty
@@ -110,7 +145,7 @@ void LinkedList<T>::AddHead(const T& data)
 template <typename T>
 void LinkedList<T>::AddTail(const T& data)
 {
-    Node* new_tail = new Node;          // New node to be added to end of list
+    Node* new_tail = new Node();          // New node to be added to end of list
     new_tail->_data = data;
 
     // Check to see if list is empty
@@ -130,3 +165,31 @@ void LinkedList<T>::AddTail(const T& data)
         _size++;                        // Increment size of linked list
     }
 }
+
+template <typename T>
+void LinkedList<T>::AddNodesHead(const T* data, unsigned int count)
+{
+    for (unsigned int i = 0; i < count; i++)
+    {
+        AddHead(data[count - i - 1]);
+    }
+}
+
+template <typename T>
+void LinkedList<T>::AddNodesTail(const T* data, unsigned int count)
+{
+    for (unsigned int i = 0; i < count; i++)
+    {
+        AddTail(data[i]);
+    }
+}
+
+template <typename T>
+unsigned int LinkedList<T>::NodeCount() const
+{
+    return _size;
+}
+
+
+
+
